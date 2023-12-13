2a3d6f546697162b431dd260522521fd7423da11
Matthew Turner <turnmas35@gmail.com>
2023-12-12 23:09:42 -0500

    working event listener on cardList

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     const list = document.getElementById("list")
+    const showCardSet = document.getElementById("show-cardset")
 
     function fetchCards() {
         fetch("https://api.tcgdex.net/v2/en/sets")
@@ -21,7 +22,22 @@ document.addEventListener("DOMContentLoaded", function() {
 
         listItem.addEventListener("click", function(e) {
             e.preventDefault();
-            
+            showCardSet.innerHTML = ""
+            const name = document.createElement("h2")
+            name.innerText = cardSet.name
+            const setId = document.createElement("h3")
+            const logo = document.createElement("img")
+            logo.width = 300
+            logo.height = 100
+            logo.src = `${cardSet.logo}.png`;
+            const symbol = document.createElement("img")
+            const cardData = document.createElement("p")
+            showCardSet.append(name, logo)
+
+            logo.addEventListener("error", function(e) {
+                e.target.src = "https://assets.tcgdex.net/en/base/base1/logo.png"
+                e.onerror = null
+            })
         })
     }
 })
