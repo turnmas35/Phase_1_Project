0f67892cd4cebc304bb1df0fc28d54a6c1dcdc14
Matthew Turner <turnmas35@gmail.com>
2023-12-15 12:32:49 -0500

    comments made

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+    
+// global variables
     const list = document.getElementById("list")
     const showCardSet = document.getElementById("show-cardset")
     const cardUl = document.getElementById("card-list")
     const cardInfo = document.getElementById("show-card-info")
     const cardForm = document.getElementById("card-form")
+    const cardSetForm = document.getElementById("cardset-form")
 
-    // function fetches card sets and displays on dom
+// function fetches card sets and displays on dom
     function fetchCardSets() {
         fetch("https://api.tcgdex.net/v2/en/sets")
         .then((resp) => resp.json())
@@ -18,11 +20,13 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     fetchCardSets()
 
+// creates li and lists card set names
     function cardSetList(cardSet) {
         const listItem = document.createElement("li")
         listItem.innerText = cardSet.name
         list.append(listItem)
 
+    // listens for click on card set name, then displays card set info for that specific card set
         listItem.addEventListener("click", function(e) {
             e.preventDefault();
             showCardSet.innerHTML = ""
@@ -44,11 +48,13 @@ document.addEventListener("DOMContentLoaded", function() {
             cardData.innerText = `Total Cards: ${cardSet.cardCount.total} | Official Cards: ${cardSet.cardCount.official}`;
             showCardSet.append(nameHeader, name, setId, logoHeader, logo, symbolHeader, symbol, cardData)
 
+        // listens for error on image display, then displays Alt image
             logo.addEventListener("error", function(e) {
                 e.target.src = "https://assets.tcgdex.net/en/base/base1/logo.png"
                 e.onerror = null
             })
 
+        // listens for error on image display, then displays Alt image
             symbol.addEventListener("error", function(e) {
                 e.target.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/2052px-Pok%C3%A9_Ball_icon.svg.png"
                 e.onerror = null
@@ -68,13 +74,13 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     fetchCards()
 
+// list cards to cardUl
     function cardList(card) {
-        // console.log(card.id, cardSet.id)
-
         const cardLi = document.createElement("li")
         cardLi.innerText = card.name
         cardUl.append(cardLi)
 
+    // listens for click on card name, the displays info for that specific card
         cardLi.addEventListener("click", function(e) {
             e.preventDefault();
             cardInfo.innerHTML = ""
@@ -98,11 +104,27 @@ document.addEventListener("DOMContentLoaded", function() {
             })
         })
     }
-
+// submit button search code
     cardForm.addEventListener("submit", function(e) {
         e.preventDefault();
         cardSearch(cardForm.children[0].value)
         console.log(cardForm.children[0].value)
+    })
+
+    function cardSearch(name) {
+        fetch(`https://api.tcgdex.net/v2/en/cards/${name}`)
+            .then(res => res.json())
+            .then(jsonCard => {
+                cardInfo.innerHTML = ""
+                cardUl.innerHTML = ""
+                cardList(jsonCard)
+            })
+    };
+
+    cardSetForm.addEventListener("submit", function(e) {
+        e.preventDefault();
+        cardSearch(cardSetForm.children[0].value)
+        console.log(cardSetForm.children[0].value)
     })
 
     function cardSearch(name) {
